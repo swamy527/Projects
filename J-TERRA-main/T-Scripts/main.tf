dfc08ee161746bd7efeb0c835aaf09c320210c2b
swamy527 <swamybandari527@gmail.com>
2025-09-04 17:49:47 +0530

    files udpated

--- J-TERRA-main/T-Scripts/main.tf
+++ J-TERRA-main/T-Scripts/main.tf
@@ -1,17 +1,13 @@
 provider "aws" {
-  region = "ap-south-1"
-  access_key= "XXXXXXXXXXXXXXX"
-  secret_key= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-  
-  
+  region = "us-east-1"
 }
 
-variable vpc_cidr_block {}
-variable subnet_1_cidr_block {}
-variable env_prefix {}
-variable avail_zone {}
-variable my_ip {}
-variable instance_type {}
+variable "vpc_cidr_block" {}
+variable "subnet_1_cidr_block" {}
+variable "env_prefix" {}
+variable "avail_zone" {}
+variable "my_ip" {}
+variable "instance_type" {}
 
 
 
@@ -38,25 +34,25 @@ output "ami_id" {
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-      Name = "${var.env_prefix}-vpc"
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_1_cidr_block
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_1_cidr_block
   availability_zone = var.avail_zone
   tags = {
-      Name = "${var.env_prefix}-subnet-1"
+    Name = "${var.env_prefix}-subnet-1"
   }
 }
 
 resource "aws_internet_gateway" "myapp-igw" {
-        vpc_id = aws_vpc.myapp-vpc.id
+  vpc_id = aws_vpc.myapp-vpc.id
 
-    tags = {
-     Name = "${var.env_prefix}-internet-gateway"
-   }
+  tags = {
+    Name = "${var.env_prefix}-internet-gateway"
+  }
 }
 
 /*resource "aws_route_table" "myapp-route-table" {
@@ -72,16 +68,16 @@ resource "aws_internet_gateway" "myapp-igw" {
    }
  } */
 
- resource "aws_default_route_table" "main-rtb" {
-    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-        Name = "${var.env_prefix}-main-rtb"
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
 }
 
 
@@ -117,27 +113,27 @@ resource "aws_default_security_group" "default-sg" {
 }
 
 
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "myapp-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYlDlr8hgHkKBNqu2m0g6qWbIXGmss5vWTc8ZjYMqxJv5sYX+kNkkeyn2wC06KSWKQ8DpAE6dGnhlsWmc2/ZXjPKjCkx6ne4axdRU1vXIeC7Dq5T9gsdilbL0oRF7UJyE8oXs6xR66i8Kdaujwo5wSMihcVIkUVUqXYZCKm+gnPQLMAUcvXO2coCR8GH4tr7SssfGsEGubDjEFFr0tOKYW2qOV7Pj/69X3Vasqys2HtegXAXWjShSb1zN3LPmMOdYLs6AG4V0wJH2geQ/i97FVSk7O5CpmYByAkXVuPbRtoXQgb1PcbhZMQi2WPDlCP3HcRoB3rnds9nYm0eKtUXQHQmiCIi1YPAdqRvpyGdfoq9S4X/YmjTgOAvwcAmK24saDTjYbmOAkjhEOh+tEtxGs/358L1QzQpatt0pCieZqyXgzpVosTWc9QoZhew0RtaZ9eVL2+rwhxavrhZU/oxpUHj50892YzsthPtRjxZOelk/H8gV7/TlFZhHTkB6V1Gk= ubuntu@ip-172-31-43-43"
+# resource "aws_key_pair" "ssh-key" {
+#   key_name   = "myapp-key"
+#   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYlDlr8hgHkKBNqu2m0g6qWbIXGmss5vWTc8ZjYMqxJv5sYX+kNkkeyn2wC06KSWKQ8DpAE6dGnhlsWmc2/ZXjPKjCkx6ne4axdRU1vXIeC7Dq5T9gsdilbL0oRF7UJyE8oXs6xR66i8Kdaujwo5wSMihcVIkUVUqXYZCKm+gnPQLMAUcvXO2coCR8GH4tr7SssfGsEGubDjEFFr0tOKYW2qOV7Pj/69X3Vasqys2HtegXAXWjShSb1zN3LPmMOdYLs6AG4V0wJH2geQ/i97FVSk7O5CpmYByAkXVuPbRtoXQgb1PcbhZMQi2WPDlCP3HcRoB3rnds9nYm0eKtUXQHQmiCIi1YPAdqRvpyGdfoq9S4X/YmjTgOAvwcAmK24saDTjYbmOAkjhEOh+tEtxGs/358L1QzQpatt0pCieZqyXgzpVosTWc9QoZhew0RtaZ9eVL2+rwhxavrhZU/oxpUHj50892YzsthPtRjxZOelk/H8gV7/TlFZhHTkB6V1Gk= ubuntu@ip-172-31-43-43"
 
-}
+# }
 
 output "server-ip" {
-    value = aws_instance.myapp-server.public_ip
+  value = aws_instance.myapp-server.public_ip
 }
 
 resource "aws_instance" "myapp-server" {
   ami                         = data.aws_ami.amazon-linux-image.id
   instance_type               = var.instance_type
-  key_name                    = aws_key_pair.ssh-key.key_name
+  key_name                    = "arun"
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.myapp-subnet-1.id
   vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
-  availability_zone                           = var.avail_zone
+  availability_zone           = var.avail_zone
 
 
-user_data = file("entrypoint.sh")
+  user_data = file("entrypoint.sh")
 
   tags = {
     Name = "${var.env_prefix}-server"
